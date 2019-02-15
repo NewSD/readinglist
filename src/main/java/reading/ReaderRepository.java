aa42f761b102a31294db331bfd5ae4fc5c9d3a22
niuhaideng <niuhaideng@163.com>
2019-02-15 12:16:56 +0800

    reader security config

---
+++ src/main/java/reading/ReaderRepository.java
@@ -0,0 +1,9 @@
+package reading;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by ami on 2019/2/15.
+ */
+public interface ReaderRepository extends JpaRepository<Reader,String> {
+}
